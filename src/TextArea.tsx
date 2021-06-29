b347122fd01558f82d66f7e4d77704c2f09d0bb8
William Wells <whanwells@gmail.com>
2021-06-28 23:06:47 -0500

    Increase textarea font size

--- src/TextArea.tsx
+++ src/TextArea.tsx
@@ -5,10 +5,7 @@ type TextAreaProps = Omit<TextAreaAttributes, "className" | "rows" | "cols">;
 
 const TextArea: VFC<TextAreaProps> = (props) => {
   return (
-    <textarea
-      {...props}
-      className="p-2 w-full h-72 border border-gray-300 text-sm sm:text-base"
-    />
+    <textarea {...props} className="p-2 w-full h-72 border border-gray-300" />
   );
 };
 

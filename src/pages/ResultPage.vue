82183df212945758c83ae79746cedfa3b3a43393
William Wells <whanwells@gmail.com>
2025-01-24 13:53:55 -0600

    Compute lines in result page

--- src/pages/ResultPage.vue
+++ src/pages/ResultPage.vue
@@ -2,21 +2,23 @@
   <div class="flex items-center justify-center p-4">
     <ul>
       <EntryListItem
-        v-for="(entry, index) in source.entries"
-        :key="index + entry"
-        :source="entry"
+        v-for="(line, index) in lines"
+        :key="index + line"
+        :source="line"
       />
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import { useRouter } from "vue-router";
 import EntryListItem from "../components/EntryListItem.vue";
 import { useSourceStore } from "../stores/source";
 
 const router = useRouter();
 const source = useSourceStore();
+const lines = computed(() => source.text.split(/\r?\n/));
 
 if (!source.text.length) {
   // If nothing was entered, odds are good someone came to this route manually.
--- src/stores/source.ts
+++ src/stores/source.ts
@@ -1,8 +1,7 @@
 import { defineStore } from "pinia";
-import { computed, ref } from "vue";
+import { ref } from "vue";
 
 export const useSourceStore = defineStore("source", () => {
   const text = ref("");
-  const entries = computed(() => text.value.split(/\r?\n/));
-  return { entries, text };
+  return { text };
 });
